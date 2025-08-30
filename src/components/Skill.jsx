d2dc7ac7563f07f9b7ee90241da92d060b352c19
Op0778 <prakashlkom5@gmail.com>
2025-08-30 21:43:32 +0530

    Update Skill.jsx

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -5,13 +5,13 @@ import { SiMongodb, SiMysql, SiCplusplus } from "react-icons/si";
 const skills = [
   { skill: "HTML", percent: 90, icon: <FaHtml5 size={80} color="#E34F26" /> },
   { skill: "CSS", percent: 85, icon: <FaCss3Alt size={80} color="#1572B6" /> },
-  { skill: "JavaScript", percent: 75, icon: <FaJs size={100} color="#F7DF1E" /> },
-  { skill: "React", percent: 70, icon: <FaReact size={100} color="#61DAFB" /> },
-  { skill: "MongoDB", percent: 50, icon: <SiMongodb size={100} color="#47A248" /> },
-  { skill: "PHP", percent: 40, icon: <FaPhp size={100} color="#777BB4" /> },
-  { skill: "SQL", percent: 40, icon: <SiMysql size={100} color="#00618A" /> },
-  { skill: "Python", percent: 80, icon: <FaPython size={100} color="#3776AB" /> },
-  { skill: "C/C++", percent: 80, icon: <SiCplusplus size={100} color="#00599C" /> },
+  { skill: "JavaScript", percent: 75, icon: <FaJs size={80} color="#F7DF1E" /> },
+  { skill: "React", percent: 70, icon: <FaReact size={80} color="#61DAFB" /> },
+  { skill: "MongoDB", percent: 50, icon: <SiMongodb size={80} color="#47A248" /> },
+  { skill: "PHP", percent: 40, icon: <FaPhp size={80} color="#777BB4" /> },
+  { skill: "SQL", percent: 40, icon: <SiMysql size={80} color="#00618A" /> },
+  { skill: "Python", percent: 80, icon: <FaPython size={80} color="#3776AB" /> },
+  { skill: "C/C++", percent: 80, icon: <SiCplusplus size={80} color="#00599C" /> },
 ];
 
 const Skills = () => {
